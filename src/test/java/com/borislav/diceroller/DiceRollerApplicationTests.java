1afe4ef1c9315b93112d991db7ce31336655ad74
borko <borko1945@gmail.com>
2021-05-27 13:21:24 +0300

    Project bootstrapping

---
+++ src/test/java/com/borislav/diceroller/DiceRollerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.borislav.diceroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiceRollerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
